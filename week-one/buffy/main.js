7e6c7bada5ed53eb95d0bd0a17f5b30778f8e815
Student Workspace <student@Candices-MacBook-Air.local>
2019-01-29 02:14:14 -0600

    git created

---
+++ week-one/buffy/main.js
@@ -0,0 +1,39 @@
+// JS Variables
+let captain = 'Ahab';
+let phrase = 'Oh ' + captain + ', my ' + captain + '!"';
+// JS Conditionals
+let souls = 3;
+let lifeRafts = 2;
+
+If(souls>lifeRafts){console.log('SOS!');};
+// Data Structures - JS Arrays
+let weekend = ['Saturday'];
+weekend.push('Sunday');
+weekend.unshift('Friday');
+let day = weekend[1];
+weekend.shift();
+
+//Data Structures - JS Objects
+
+let brain = {
+    energyLevel: 10,
+    
+};
+
+let energy = brain.energyLevel;
+
+brain.dream = 'electric sheep';
+
+//JS Functions
+
+function computeArea (length, width){
+    let area = length * width;
+    return area;
+}
+
+let area = computeArea(3, 4);
+
+
+
+
+
